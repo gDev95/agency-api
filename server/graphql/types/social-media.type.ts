48896bab35c799041bda49f0715b82e147675603
gDev95 <paulmeyerber@gmail.com>
2020-07-10 11:01:49 +0200

    hotfix social media input type can be nullable

--- server/graphql/types/social-media.type.ts
+++ server/graphql/types/social-media.type.ts
@@ -2,7 +2,7 @@ import {
 	GraphQLObjectType,
 	GraphQLString,
 	GraphQLInputObjectType,
-	GraphQLNonNull
+	GraphQLNonNull,
 } from "graphql";
 
 export const SocialMediaType = new GraphQLObjectType({
@@ -12,17 +12,17 @@ export const SocialMediaType = new GraphQLObjectType({
 		instagram: { type: GraphQLString },
 		soundCloud: { type: GraphQLString },
 		beatport: { type: GraphQLString },
-		residentAdvisor: { type: GraphQLString }
-	})
+		residentAdvisor: { type: GraphQLString },
+	}),
 });
 
 export const SocialMediaInput = new GraphQLInputObjectType({
 	name: "SocialMediaInput",
 	fields: () => ({
-		facebook: { type: GraphQLNonNull(GraphQLString) },
-		instagram: { type: GraphQLNonNull(GraphQLString) },
-		soundCloud: { type: GraphQLNonNull(GraphQLString) },
-		beatport: { type: GraphQLNonNull(GraphQLString) },
-		residentAdvisor: { type: GraphQLNonNull(GraphQLString) }
-	})
+		facebook: { type: GraphQLString },
+		instagram: { type: GraphQLString },
+		soundCloud: { type: GraphQLString },
+		beatport: { type: GraphQLString },
+		residentAdvisor: { type: GraphQLString },
+	}),
 });
